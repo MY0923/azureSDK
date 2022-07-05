4280b9f588641e60a90fa2fd313cadcc8d793601
MY0923 <hal.osaka.90427@gmail.com>
2022-07-05 18:30:53 +0900

    function

--- storageBlob.js
+++ storageBlob.js
@@ -15,22 +15,18 @@ async function main() {
 
     // コンテナの名前を作成(uniqueにする。小文字でしか作れない)
     // const containerName = "quickstart" + uuidv1();
-    const containerName = "test6789012";
-    
+    const containerName = "aiueo";
+
     console.log("\nコンテナ作成中");
     console.log("\t", containerName);
 
+
     // コンテナクライアントを取得
     const containerClient = getContainerClient(blobServiceClient, containerName);
-    
 
     // コンテナ作成
-    // const publicAccessType = "blob";
-    const containerOptions = {
-        access: "blob",
-    }
-    // const containerResponse = createContainerResponse(containerOptions, containerClient);
-    createContainerResponse(containerClient, containerOptions);
+    // const containerResponse = createContainerResponse(containerClient, containerOptions);
+    createContainerResponse(containerClient);
 
 
     // blobに名前作成(uniqueにする)
@@ -39,61 +35,23 @@ async function main() {
     const contentType = mime.getType(blobName);
     const options = {
         blobHTTPHeaders: {
-        blobContentType: contentType,
+            blobContentType: contentType,
         },
     };
 
-    // blob clientを取得
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    console.log("\nUploading to Azure storage as blob:\n\t", blobName);
+    const blockBlobClient = getBlobClient(containerClient, blobName);
 
+    
     // blobにデータをアップロード
-    const data = fs.readFileSync('index.html', "utf-8");
-    const uploadBlobResponse = await blockBlobClient.upload(data, data.length, options);
-    console.log(
-        "Blob was uploaded successfully. requestId: ",
-        uploadBlobResponse.requestId
-    );
+    const uploadBlobResponse = blobFileUpload(blockBlobClient ,blobName, options);
 
-    // コンテナ内のblobをリスト表示
-    console.log("\nListing blobs...");
-    for await (const blob of containerClient.listBlobsFlat()) {
-        console.log("\t", blob.name);
-    }
-
-    //最初から最後までのBlobを取得する
-    // NodeはdownloadBlockBlobResponse.readableStreamBodyにアクセスしてダウンロードしたデータを取得
-    //ブラウザは、 downloadBlockBlobResponse.readableStreamBodyにアクセスし、ダウンロードしたデータを取得
-    const downloadBlockBlobResponse = await blockBlobClient.download(0);
-    console.log("\nDownloaded blob content...");
-    console.log(
-        "\t",
-        await streamToText(downloadBlockBlobResponse.readableStreamBody)
-    );
 }
 
-// streamをtextに変換
-async function streamToText(readable) {
-    readable.setEncoding('utf8');
-    let data = '';
-    for await (const chunk of readable) {
-        data += chunk;
-    }
-    return data;
-}
 
-// コンテナ削除
-// console.log("\nDeleting container...");
-
-// const deleteContainerResponse = await containerClient.delete();
-// console.log(
-//     "Container was deleted successfully. requestId: ",
-//     deleteContainerResponse.requestId
-// );
 
 
 // Azure storageにアクセスできるか確認(接続文字列を使う)
-function storageCheck(){
+function storageCheck() {
     if (!process.env.ACCESS_KEY) {
         throw Error("Azure Storage Connection string not found");
     }
@@ -101,18 +59,34 @@ function storageCheck(){
 }
 
 // コンテナクライアント作成に使用するBlobServiceClientオブジェクト作成
-function blobServiceClientCreate(accessKey){
+function blobServiceClientCreate(accessKey) {
     return BlobServiceClient.fromConnectionString(accessKey);
 }
 
 // コンテナクライアントを取得
-function getContainerClient(blobServiceClient, containerName){
+function getContainerClient(blobServiceClient, containerName) {
     return blobServiceClient.getContainerClient(containerName);
 }
 
 //コンテナ作成(同じコンテナ名があると処理が失敗する)
-async function createContainerResponse(containerOptions, containerClient){
+async function createContainerResponse(containerClient) {
+    const containerOptions = {
+        access: "blob",
+    }
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     return await containerClient.create(containerOptions);
+}
+
+// blob clientを取得
+function getBlobClient(containerClient, blobName) {
+    console.log("\nUploading to Azure storage as blob:\n\t", blobName);
+    return containerClient.getBlockBlobClient(blobName);
+}
+
+async function blobFileUpload(blockBlobClient ,blobName, options){
+    // blobにデータをアップロード
+    const data = fs.readFileSync('index.html', "utf-8");
+    return await blockBlobClient.upload(data, data.length, options);
 }
 
 main()
